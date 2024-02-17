e928d8f935352acb2381fb903cfe187afd60224d
ArcHighwind <105253743+ArcHighwind@users.noreply.github.com>
2024-02-17 03:21:55 -0600

    starting

---
+++ examen1_parte2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {Home} from './components/Home'
+import './styles.css'
+
+function App() {
+
+  return (
+    <>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
